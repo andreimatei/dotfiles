89ea3bf8fd54d3e8ad04b1e27ddc38271a6ba1a5
Andrei Matei <andrei@cockroachlabs.com>
2021-02-07 19:33:12 -0500

    improve setup-host

--- scripts/setup-host.sh
+++ scripts/setup-host.sh
@@ -9,34 +9,64 @@ mkdir -p ~/.swp
 # !!! install Vundle for Vim
 # git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # !!! if ui, sudo apt-get install wmctrl
-# !!! fix path to the findwindow script in keyboard shortcuts
 
 # !!! ccache, bazel, gcloud
 # sudo apt-get install ccache
 # snap install google-cloud-sdk --classic
 
-# Clone dotfiles.
-# !!! don't fail if dir already exists
-git clone https://github.com/andreimatei/dotfiles.git ~/dotfiles
-# !!! edit origin remote to use ssh
+DOTFILES_DIR=~/dotfiles
+
+clone_or_pull() {
+  local remote=$1
+  local dir=$2
+  local branch_arg=""
+  local branch_name=""
+  if [ ! -z ${3:-} ]; then
+    branch_arg="--branch $3"
+    branch_name=$3
+  fi
+  if [ ! -d $dir ]; then
+    echo "Cloning $remote"
+    git clone $branch_arg $remote $dir
+  else
+    echo "Updating dotfiles"
+    git --git-dir=$dir/.git pull origin $branch_name
+  fi
+}
+
+clone_or_pull https://github.com/andreimatei/dotfiles.git ~/dotfiles
+
+install_symlink() {
+  file=$1
+  if [ -L ~/$file ]; then
+    echo "~$file alredy a symlink. Not touching." 
+    return
+  fi
+  if [ -f ~/$file ]; then
+    echo "Backing up $file to $file.bk"
+    mv ~/$file ~/$file.bk
+  fi
+  echo "Installing ~/$file"
+  ln -s ~/dotfiles/dotfiles/$file ~/$file
+}
 
 # Create symlinks to all the dotfiles.
-for filename in $(find ~/dotfiles/dotfiles -type f -printf "%f "); do
-  echo "installing $filename"
-  # !!! make backup copies
-  ln -s -f ~/dotfiles/dotfiles/$filename ~/$filename
+for file in $(find ~/dotfiles/dotfiles -type f -printf "%f "); do
+  install_symlink $file
 done
 
+# Install zsh and make it the login shell.
 sudo apt install zsh
+chsh -s /usr/bin/zsh
 
 # Install Liquidprompt.
-git clone --branch stable https://github.com/nojhan/liquidprompt.git ~/src/liquidprompt
+clone_or_pull https://github.com/nojhan/liquidprompt.git ~/src/liquidprompt stable
 
 # Install Oh My Zsh.
-ZSH=~/src/oh-my-zsh RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ZSH=~/src/oh-my-zsh RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || true
 
 # Install zsh-autosuggestions as a oh-my-zsh plugin.
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/src/oh-my-zsh/custom/plugins/zsh-autosuggestions
+clone_or_pull https://github.com/zsh-users/zsh-autosuggestions ~/src/oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 dconf load / < ~/dotfiles/config/linux/dconf.backup
 
